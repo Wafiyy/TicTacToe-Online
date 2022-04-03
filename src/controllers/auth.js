d8c9c520708fa79d268b54a89e58617e509acb86
Wafiyy <85643076+wafiyy@users.noreply.github.com>
2022-04-03 06:29:00 +0500

    Add database

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,11 +1,14 @@
 const { reject } = require("../util")
-
+require("../db")
 function create(req,res){
   const { username } = req.body
-  
-  if(!(/^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$/.test(username))){
+
+  if(!(/^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$/.test(username||""))){
     reject(res,"Invalid Username")
   }
-  
 
+
+}
+module.exports = {
+  create
 }
---
+++ src/db/index.js
@@ -0,0 +1,23 @@
+process.db = {}
+process.db.users = {
+    "121": [
+        {
+            username: "Abror",
+            status: "player",
+            isTurn: false,
+        },
+        {
+            username: "Ali",
+            status: "player",
+            isTurn: true,
+        },
+        {
+            username: "Alisher",
+            status: "viewer",
+            isTurn: null,
+        }
+    ]
+}
+process.db.playground = {
+    "121": ["","","","","","","","",""]
+}
